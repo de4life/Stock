74aa865767a5ca5cfd0de82d2b277a9f7186d843
de4life <ageev_artur@ukr.net>
2023-02-10 12:25:53 +0200

    Initial Commit

---
+++ Stock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stock
+//
+//  Created by Артур Агеев on 10.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stock/StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockApp.swift
+//  Stock
+//
+//  Created by Артур Агеев on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
